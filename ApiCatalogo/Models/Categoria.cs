aa0af6300617c3274327ecfc87fb1f3d1cca6a7c
Deivis_vargas <deivisvp@yahoo.com.br>
2023-11-08 14:05:02 -0300

    Adicionar arquivos de projeto.

---
+++ ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCatalogo.Models;
+
+
+[Table("Categorias")]
+public class Categoria
+{
+
+    //inicialisando a coleção de  produtos no construtor da classe 
+    public Categoria() {
+        Produtos = new Collection<Produto>();
+    }
+
+    [Key]
+    public int CategoriaId { get; set; }
+
+    //data Anotations para alterar os padrões dp EF core 
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? ImageUrl { get; set; }
+
+    //definindo a relação de produto com categoria onde uma categoria pode ter muitos 
+    //produtos
+    public ICollection<Produto>? Produtos { get; set; }
+
+}
---
+++ ApiCatalogo/Models/Produto.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCatalogo.Models;
+
+
+[Table("Produtos")]
+public class Produto
+{
+    [Key] 
+    public int ProdutoId { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? Descricao { get; set; }
+
+
+    //especifica a precisão da coluna 
+    [Column(TypeName ="decimal(10,2)")]
+    [Required]
+    public decimal? Preco { get; set;}
+
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set;}
+
+    public float? Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+
+    //cria a chave estrangeira da tabela Categoria
+    public int? CategoriaId { get; set; }
+
+    //Propriedade de navegação
+    public Categoria? Categoria { get; set; }   
+
+}
