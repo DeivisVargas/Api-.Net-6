8332d168414be03c8473cfa18242dfd098667f1a
Deivis_vargas <deivisvp@yahoo.com.br>
2023-11-10 17:00:05 -0300

    rotas para produtos concluida

---
+++ ApiCatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,14 @@
+﻿
+using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+    }
+}
--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using ApiCatalogo.Context;
 using ApiCatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalogo.Controllers
 {
@@ -32,7 +33,7 @@ namespace ApiCatalogo.Controllers
         }
 
         //rota de pesquisa de produtos por id definindo inteiro 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}" , Name ="ObterProduto")] // rota nomeada 
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault( p => p.ProdutoId == id);
@@ -42,5 +43,68 @@ namespace ApiCatalogo.Controllers
             }
             return produto;
         }
+
+
+        //rota POST para inserir o produto 
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+
+            //espera receber os dados no padrão do modelo
+            _context.Produtos.Add(produto); //inclui no contexto sem inserir no banco
+            _context.SaveChanges(); // sauva na tabela do banco 
+
+            //retorna status 201 e chama a rota de consultar ObterProduto por id
+            //para devolver os dados do produto 
+            return new CreatedAtRouteResult
+                (
+                    "ObterProduto",
+                    new { id = produto.ProdutoId } ,
+                    produto
+                );
+
+
+
+        }
+
+        //Atualiza produto aponas uma atualização completa , se passar somente uma informação por exemplo
+        //não vai funcionar 
+        [HttpPut("{id:int}", Name = "AlterarProduto")]
+        public ActionResult Put(int id ,Produto produto)
+        {
+            if ( id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            //fala ao contexto que o produto vai ser alterado 
+            //no contexto desconectado
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges(); //salva os dados
+
+            //return NoContent(); retorna um ok mas sem os dados do produto
+
+            //retorna os dados do produto
+            return Ok(produto);
+
+        }
+
+        [HttpDelete("{id:int}", Name ="DeletarProduto")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+                return NotFound("Produto não localizado...");
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        
+        
+        }
+        
     }
 }
